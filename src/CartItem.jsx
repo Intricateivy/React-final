229f56a7eb16f7fa8049e709db4f273702b7822b
Intricateivy <D.Inkfox@workmail.com>
2025-05-16 19:28:52 -0400

    Added checkout alert

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -43,8 +43,8 @@ const CartItem = ({ onContinueShopping }) => {
         return totalCost.toFixed(2);} 
   };
 
-  const handleCheckoutShopping = (e) => {
-    alert('Functionality to be added for future reference');
+  const handleCheckoutShopping = () => {
+    alert('Not yet available!!');
   };
 
   return (
@@ -72,7 +72,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">N/A</button>
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
       </div>
     </div>
   );
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,7 +7,7 @@ import {addItem} from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addedToCart, setAddedToCart] = useState({});
+    const [addedToCart, setAddedToCart] = useState(false);
 
     const dispatch = useDispatch();
     const cartItems = useSelector((state) => state.cart.items);
