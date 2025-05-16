41ad88ab692dc155fc8374ad412fae9f49ea382f
Intricateivy <D.Inkfox@workmail.com>
2025-05-15 20:16:26 -0400

    Finally got add to cart working

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -5,10 +5,18 @@ export const CartSlice = createSlice({
   initialState: {
     items: [], // Initialize items as an empty array
   },
+
   reducers: {
     addItem: (state, action) => {
-    
+        const {name, image, cost} = action.payload;
+        const exsistingItem = state.items.find(item => item.name === name);
+        if (exsistingItem) {
+            exsistingItem.quantity += 1;
+        } else {
+            state.items.push({name, image, cost, quantity: 1});
+        }
     },
+    
     removeItem: (state, action) => {
     },
     updateQuantity: (state, action) => {
